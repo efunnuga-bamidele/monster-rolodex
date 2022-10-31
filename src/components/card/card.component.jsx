d777e876179d4675293d3f8564bbfb1a6f0339f7
Efunnuga Bamidele <bamideleefunnuga9@gmail.com>
2022-10-31 15:36:45 +0100

    Implement Card Component

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,20 @@
+import { Component } from 'react'
+
+//styles
+import './card.style.css'
+
+class Card extends Component{
+    render(){
+        const { name, email, id } = this.props 
+        return(
+
+            <div className="card-container" key={id}>
+                <img src={`https://www.robohash.org/${id}?set=set2&size=180x180`} alt={`monster ${name}`} />
+                <h2>{name}</h2>
+                <p>{email}</p>
+            </div>
+        )
+    }
+}
+
+export default Card
